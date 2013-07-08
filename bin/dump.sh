5c1e2a141d317674efbf614ca0d8c66483246a85
jenselme <julien.enselme@centrale-marseille.fr>
2013-07-08 21:28:27 +0200

    dump.sh
    Utilise mysqldump pour sauvegarder toutes les bases de données drupal 7
    si jamais drush est en panne.

--- bin/dump.sh
+++ bin/dump.sh
@@ -2,6 +2,11 @@
 
 madate=`date "+%Y-%m-%d-%Hh%Mm%Ss"`
 
+#Dump D6 database with webassos user.
 /usr/local/bin/mysqldump --single-transaction webassos -h myweb.serv.int -u webassos --password=HBVH2ljgyZCA0AP251DY > /users/guest/assos/Desktop/dump_d6/webassos.dump$madate.sql
+
+#Dump D6 form database with forum user.
 /usr/local/bin/mysqldump forum -h myweb.serv.int -u forum --password=dtcAltF12 > /users/guest/assos/Desktop/dump_d6/forum.dump$madate.sql
-/usr/local/bin/mysqldump sudfinance -h myweb.serv.int -u sudfinance --password=JLSMiisuRxrjLmqT2A7R > /users/guest/assos/Desktop/dump_d6/sudfinance.dump$madate.sql
+
+#Dump D7 databases at once.
+/usr/local/bin/mysqldump -h myassos.serv.int -u assos -pNoNo82jJ --all-databases > /users/guest/assos/Desktop/dump_d7/d7_all$madate.sql

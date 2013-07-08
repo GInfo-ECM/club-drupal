5f0aa9eb2b0752a17d3c6482ceb2ff4195ed886d
jenselme <julien.enselme@centrale-marseille.fr>
2013-07-08 21:30:00 +0200

    maj_d7
    Ajout de log grâce à drushall_atest_logged

--- bin/maj_d7.sh
+++ bin/maj_d7.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 PATH=/usr/local/bin:/usr/bin:/bin
 
-
 ##############
 # nlehuby - 21 mai 2012
 # ce script permet d'effectuer les mises à jour des projets pour l'installation drupal 7
@@ -9,14 +8,21 @@ PATH=/usr/local/bin:/usr/bin:/bin
 
 #Lancer le cron pour que les sites sachent s'il y a des majs à faire
 /users/guest/assos/bin/drushall_atest -q cron 
+
 #Supprimer le cache pour réduire de moitié la taille des bases de données sauvegardées 
 /users/guest/assos/bin/drushall_atest cc all
+
 #Script de sauvegarde des bases de données
 /bin/sh /users/guest/assos/bin/dump_site_atest_all
+
 #Mettre à jour le code des modules et thèmes tiers
-/users/guest/assos/bin/drushall_atest --no-core -y upc 
-/users/guest/assos/bin/drushall_atest -y updb
+/users/guest/assos/bin/drushall_atest_logged --no-core -y upc 
+
+#Update all databases to match current modules code.
+/users/guest/assos/bin/drushall_atest_logged -y updb
+
 #Rapport du cron
 /users/guest/assos/bin/drushall_atest cron | mail -s "Rapport cron" assos@centrale-marseille.fr
+
 #Rapport sur la taille utilisée du disque
 /bin/sh /users/guest/assos/bin/taille.sh | mail -s "Rapport utilisation disque" assos@centrale-marseille.fr 

8f9b0d954a86f944080593279dfa07359f74408f
Julien Enselme <julien.enselme@centrale-marseille.fr>
2013-09-08 22:30:56 +0200

    Création des settings.local.php
    Petites modifications afin que le script fonctionne.

--- scripts_divers/d7-all-gen-settings-local.sh
+++ scripts_divers/d7-all-gen-settings-local.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-. /users/guest/assos/bin/script-config.sh
+. /users/guest/assos/bin/scripts-config.sh
 . /users/guest/assos/bin/scripts-utils.sh
 
 # This script generate settings.php and settings.local.php from settings.php
 
 for settings in $(find $d7_dir_sites -mindepth 2 -maxdepth 2 -name settings.php) ; do
     # Get infos from settings.php
-    d7_site_name=$(grep "^\s*'database' => '\(.*\)'" $settings | sed "s/^ *'database' => '\(.*\)'.*/\1/")
+    db_name=$(grep "^ *'database' => '\(.*\)'" $settings | sed "s/^ *'database' => '\(.*\)'.*/\1/")
     db_user=$(grep "^ *'username' => '\(.*\)'" $settings | sed "s/^ *'username' => '\(.*\)'.*/\1/")
-    db_password=$(grep "^\s*'password' => '\(.*\)'" $settings | sed "s/^ *'password' => '\(.*\)'.*/\1/")
-    base_url=$(grep "^\s*\$base_url = '\(.*\)'" $settings | sed "s/^ *\$base_url = '\(.*\)'.*/\1/")
+    db_password=$(grep "^ *'password' => '\(.*\)'" $settings | sed "s/^ *'password' => '\(.*\)'.*/\1/")
+    base_url=$(grep "^ *\$base_url = '\(.*\)'" $settings | sed "s/^ *\$base_url = '\(.*\)'.*/\1/")
 
     settings_dir=`give_dir $settings`
 
@@ -20,11 +20,12 @@ for settings in $(find $d7_dir_sites -mindepth 2 -maxdepth 2 -name settings.php)
 
     # We go into the settings.php directory.
     cd $settings_dir
+    pwd
 
     cp $d7_settings settings.php
     chmod 400 settings.php
 
-    generate_settings_local $d7_site_name $site_password $d7_settings_local_template settings.local.php
+    sed "s/\%\%DBUSER\%\%/$db_user/ ; s/\%\%DBNAME\%\%/$db_name/ ; s/\%\%DBPASS\%\%/$db_password/ ; s#\%\%BASE_URL\%\%#$base_url#" < ~/tmp/d7-settings-local-template.php > settings.local.php
 
     chmod 400 settings.local.php
 done
---
+++ scripts_divers/d7-settings-local-template.php
@@ -0,0 +1,19 @@
+<?php
+
+$databases = array (
+  'default' =>
+  array (
+    'default' =>
+    array (
+      'database' => '%%DBNAME%%',
+      'username' => '%%DBUSER%%',
+      'password' => '%%DBPASS%%',
+      'host' => 'myassos.serv.int',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
+
+$base_url = '%%BASE_URL%%';

be958c5c963454d65221af5d7b667ab0e8b5f8fe
Julien Enselme <julien.enselme@centrale-marseille.fr>
2013-10-10 20:05:59 +0200

    d7-all-who-is-admin.sh : création

---
+++ bin/d7-all-who-is-admin.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+. /users/guest/assos/bin/scripts-config.sh
+
+for dir in `find $d7_dir_sites -maxdepth 1 -mindepth 1 -type d ! -name all | sort` ; do
+    cd $dir
+    echo $dir
+    drush sqlq "SELECT name, mail FROM users WHERE uid IN (SELECT uid FROM users_roles WHERE rid=3);" 
+done

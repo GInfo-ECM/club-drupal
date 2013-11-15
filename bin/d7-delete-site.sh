d9dbe09c41bbba13952bde88242dfc2c27df1e11
Julien Enselme <julien.enselme@centrale-marseille.fr>
2013-11-15 19:20:54 +0100

    d7-delete-site.sh: devient verbeux

--- bin/d7-delete-site.sh
+++ bin/d7-delete-site.sh
@@ -10,27 +10,28 @@ check_arguments $# 1 "$help"
 
 db_password=`ask_password_db $db_server $db_user`
 
-# Delete database.
+echo 'Delete database.'
 mysql -h $db_server -u $db_user -p$db_password -e "DROP DATABASE $d7_site_name"
 
-# Delete MYSQL user.
+echo 'Delete MYSQL user.'
 mysql -h $db_server -u $db_user -p$db_password -e "DROP USER '$d7_site_name'@'%'"
 
-# Delete site's folder.
+echo "Delete site's folder."
 chmod -R 700 $d7_site_dir
 rm -r $d7_site_dir
 
 # Delete symbolic link.
 rm $d7_dir/$d7_site_name
 
+echo 'Remove site line from sites.php'
 chmod +w $sites_php
 grep -sv "^\$.*$d7_site_name';$" $sites_php > $sites_php
 chmod 400 $sites_php
 
-# Remove site alias from aliases.drushrc.php
+echo 'Remove site alias from aliases.drushrc.php'
 grep -sv "$d7_site_name'" $aliases_drushrc_php > $aliases_drushrc_php
 
-# Delete database backups.
+echo 'Delete database backups.'
 rm -r $d7_dir_individual_auto_backup/assos.centrale-marseille.fr.$d7_site_name
 rm -r $d7_dir_individual_manual_backup/assos.centrale-marseille.fr.$d7_site_name
 

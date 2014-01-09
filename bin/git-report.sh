0c16dce52044d84cf6b11752ff2543e887dd2628
Julien Enselme <julien.enselme@centrale-marseille.fr>
2014-01-09 18:15:33 +0100

    check_if_work_tree_clean was renamed in work_tree_clean, mail requires an email address

--- bin/git-report.sh
+++ bin/git-report.sh
@@ -3,13 +3,13 @@
 . /users/guest/assos/bin/scripts-config.sh
 . /users/guest/assos/bin/scripts-utils.sh
 
-if ! `check_if_work_tree_clean` ; then
+if ! `work_tree_clean` ; then
     mail_unclean_work_tree "[git] WORK TREE UNCLEAN"
 fi
 
 git_log=`git log -p --after=yesterday`
 if [ -n "$git_log" ] ; then
-    echo $git_log | mail -s "[git] Report"
+    echo $git_log | mail -s "[git] Report" $email_multi_assos
 fi
 
 git pull --rebase

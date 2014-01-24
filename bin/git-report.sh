64a65178a2c4f2dc31ced8430d16714a960ac40f
Enselme Julien <jenselme@mourepiane143.etu.ec-m.fr>
2014-01-24 19:01:26 +0100

    Mail git repport is prettier.

--- bin/git-report.sh
+++ bin/git-report.sh
@@ -7,7 +7,7 @@ if ! `work_tree_clean` ; then
     mail_unclean_work_tree "[git] WORK TREE UNCLEAN"
 fi
 
-git_log=`git log -p --after=yesterday`
+git_log=`git log -p --since=yesterday -n 1 --pretty=format:"########         %s       ########%nDate: %ai%nby %cn %n"`
 if [ -n "$git_log" ] ; then
     echo $git_log | mail -s "[git] Report" $email_multi_assos
 fi

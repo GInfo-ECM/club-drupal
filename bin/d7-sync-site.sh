c9d0efbc2d1863c42815dbcfc34076122197ed0d
Julien Enselme <julien.enselme@centrale-marseille.fr>
2014-03-20 23:54:10 +0100

    d7-sync-site.sh works.
    - Backup the correct database.
    - We test if the site is new (no database).
    - Tests on strings are done correctly.
    - We add the option --prod to disable maintenance mode.

--- bin/d7-sync-site.sh
+++ bin/d7-sync-site.sh
@@ -8,7 +8,7 @@ a already working site. It relies on bash, drush and drush aliases.
 Before synching the ssite, the destination site's database is backuped. If the
 destination site does not exist, it is created.
 
-usage: d7-sync.sh SOURCE_SITENAME DEST_SITENAME
+usage: d7-sync.sh SOURCE_SITENAME DEST_SITENAME [--prod]
 EOF
 
 . /users/guest/assos/bin/scripts-config.sh
@@ -18,37 +18,46 @@ EOF
 if ! site_exists $2 ; then
     echo "$2 does not exit. We will create it"
     d7-create-site.sh $2 --no-init-database
+    # if the site is new, there is no database
+    new_site=1
 fi
 
 # Backup the database of SOURCE_SITE
 current_date=$(date "+%Y-%m-%d-%Hh%Mm%Ss")
 if [ $1 = "default" ] ; then
-    dir=$1
+    dir=$2
 else
-    dir=assos.centrale-marseille.fr.$1
+    dir=assos.centrale-marseille.fr.$2
 fi
-drush -y @$1 sql-dump | gzip  > $d7_dir_individual_manual_backup/$dir/$current_date.$dir.sql.gz
+if [ -z "$new_site" ] ; then
+    drush -y @$1 sql-dump | gzip  > $d7_dir_individual_manual_backup/$dir/$current_date.$dir.sql.gz
+fi
 
 # Sync files
 drush -y rsync --delete --exclude="*.php" @${1}:%site @${2}:%site
 
 # Sync databases
 ## Save file system
-private_path=$(drush @$2 vget --format=string file_private_path 2> /dev/null)
-public_path=$(drush @$2 vget --format=string file_public_path 2> /dev/null)
-temp_path=$(drush @$2 vget --format=string file_temporary_path 2> /dev/null)
+if [ -z "$new_site" ] ; then
+    private_path=$(drush @$2 vget --format=string file_private_path 2> /dev/null)
+    public_path=$(drush @$2 vget --format=string file_public_path 2> /dev/null)
+    temp_path=$(drush @$2 vget --format=string file_temporary_path 2> /dev/null)
+fi
 ## Sync
-drush sql-sync @$1 @$2
+drush -y sql-sync @$1 @$2
 ## Restore file system
-if [ -n $private_path ] ; then
+if [ -n "$private_path" ] ; then
     drush -y @$2 vset file_private_path $private_path
 fi
-if [ -n $public_path ] ; then
+if [ -n "$public_path" ] ; then
     drush -y @$2 vset file_public_path $public_path
 fi
-if [ -n $temp_path ] ; then
+if [ -n "$temp_path" ] ; then
     drush -y @$2 vset file_temporary_path $temp_path
 fi
 
-# Finish sync
-drush -y vset maintenance_mode 0
+if [ "$3" = "--prod" ] ; then
+    drush -y @$2 vset maintenance_mode 0
+else
+    drush -y @$2 vset maintenance_mode 1
+fi

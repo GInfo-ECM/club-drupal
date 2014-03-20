fa3809ffd66663842c9d580b844789f4ef8cd179
Julien Enselme <julien.enselme@centrale-marseille.fr>
2014-03-20 14:48:18 +0100

    Take into account drush aliases for #374.

--- other-scripts/one-use/move-assos.sh
+++ other-scripts/one-use/move-assos.sh
@@ -5,7 +5,16 @@ This script takes 2 arguments: the old and new location of assos' bin directory.
 all occurences of the former emplacement are remplaced by the new one.
 EOF
 
-for file in $(ls $1/bin/*.sh) ; do
-    sed "s#$1#$2#" > $file.tmp
-    mv $file.tmp $file
+correct_path() {
+    sed "s#$1#$2#" < $3 > $3.tmp
+    mv $3.tmp $3
+}
+
+
+# Bin
+for file in $(ls bin/*.sh) ; do
+   correct_path $1 $2 $file
 done
+
+# drush aliases
+correct_path $1 $2 .drush/aliases.drushrc.php

2d80eb66f02348a4839d2e8ba42eaa158254fb08
Assos Assos <assos+forge@centrale-marseille.fr>
2014-03-23 17:56:31 +0100

    Deletion of site: listerik

--- .drush/aliases.drushrc.php
+++ .drush/aliases.drushrc.php
@@ -44,7 +44,6 @@ $aliases['d7'] = array(
         'assos.centrale-marseille.fr/jenselmetest',
         'assos.centrale-marseille.fr/jpennec',
         'assos.centrale-marseille.fr/ksi',
-        'assos.centrale-marseille.fr/listerik',
         'assos.centrale-marseille.fr/mdv',
         'assos.centrale-marseille.fr/mmenossi',
         'assos.centrale-marseille.fr/phytv',
@@ -92,7 +91,6 @@ $aliases['jenselme'] = array('uri' => 'assos.centrale-marseille.fr/jenselme', 'r
 $aliases['jenselmetest'] = array('uri' => 'assos.centrale-marseille.fr/jenselmetest', 'root' => '/home/assos/drupal7/', );
 $aliases['jpennec'] = array('uri' => 'assos.centrale-marseille.fr/jpennec', 'root' => '/home/assos/drupal7/', );
 $aliases['ksi'] = array('uri' => 'assos.centrale-marseille.fr/ksi', 'root' => '/home/assos/drupal7/', );
-$aliases['listerik'] = array('uri' => 'assos.centrale-marseille.fr/listerik', 'root' => '/home/assos/drupal7/', );
 $aliases['mdv'] = array('uri' => 'assos.centrale-marseille.fr/mdv', 'root' => '/home/assos/drupal7/', );
 $aliases['mmenossi'] = array('uri' => 'assos.centrale-marseille.fr/mmenossi', 'root' => '/home/assos/drupal7/', );
 $aliases['phytv'] = array('uri' => 'assos.centrale-marseille.fr/phytv', 'root' => '/home/assos/drupal7/', );

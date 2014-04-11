f7b1ec4f8193fa4c9bb59d2136b105f259c788d4
Assos Assos <assos+forge@centrale-marseille.fr>
2014-04-11 16:30:18 +0200

    Prevent double quotes interpretation for drush command arguments. See #378.

--- bin/d7-all-drush.sh
+++ bin/d7-all-drush.sh
@@ -7,8 +7,12 @@ help="# ARGS: drush_command"
 
 check_arguments $# 1 "$help"
 
+#Store the drush command name (sqlq, vget, cron). This is the first argument. 
+drush_command_name="$1"
+#shift will remove the fist argument so that $* will have the second, third...arguments.
+shift
 for dir in $(find $d7_dir_sites -maxdepth 1 -mindepth 1 -type d ! -name all | sort) ; do
     cd $dir
     echo $dir
-    drush $*
+    drush  $drush_command_name """$*"""
 done

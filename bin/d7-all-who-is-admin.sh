b92956102a1e85485d08b406af5da2a64e9d7b48
Florent Torregrosa <florent.torregrosa@centrale-marseille.fr>
2014-04-12 10:41:19 +0200

    Remove d7-all-who-is-admin.sh, now useless because #378 solved passing sqlq command to d7-all-drush.sh

--- bin/d7-all-who-is-admin.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-. /home/assos/bin/scripts-config.sh
-
-for dir in $(find $d7_dir_sites -maxdepth 1 -mindepth 1 -type d ! -name all | sort) ; do
-    cd $dir
-    echo $dir
-    drush sqlq "SELECT name, mail FROM users WHERE uid IN (SELECT uid FROM users_roles WHERE rid=3);"
-done

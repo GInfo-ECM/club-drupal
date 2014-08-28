349952e5fd9221bc869db65073b1f96ad73a69cd
Julien Enselme <julien.enselme@centrale-marseille.fr>
2014-08-28 21:10:38 +0200

    git-report.sh: sends no email.
    The forge sends the email containing the modification. This report is unuseful.

--- bin/git-report.sh
+++ bin/git-report.sh
@@ -7,10 +7,5 @@ if ! work_tree_clean ; then
     mail_unclean_work_tree "[git] WORK TREE UNCLEAN"
 fi
 
-git_log=$(git log -p --since="yesterday" --pretty=format:"########## %s ###########")
-if [ -n "$git_log" ] ; then
-    echo "$git_log" | mail -s "[git] Report" $email_multi_assos
-fi
-
-git pull --rebase > /dev/null
-git push > /dev/null
+git pull --rebase > /dev/null 2>&1
+git push > /dev/null 2>&1

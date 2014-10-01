5bee87a1e270c75c023e2c7e4f6172edb9280249
Julien Enselme <julien.enselme@centrale-marseille.fr>
2014-10-01 14:57:55 +0200

    d7-all-drush-sleep.sh only execute cron with a pause of a given time.

--- bin/d7-all-drush-sleep.sh
+++ bin/d7-all-drush-sleep.sh
@@ -10,6 +10,6 @@ check_arguments $# 1 "$help"
 for dir in $(find $d7_dir_sites -maxdepth 1 -mindepth 1 -type d ! -name all | sort) ; do
     cd $dir
     echo $dir
-    drush "$@"
-    sleep 60
+    drush -q cron
+    sleep $1
 done

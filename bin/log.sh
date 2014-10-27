e451b022110aa64312c008cbc87663d49c969650
Julien Enselme <julien.enselme@centrale-marseille.fr>
2014-10-27 16:53:22 +0100

    Modify log.sh and .unlog to use group.

--- bin/log.sh
+++ bin/log.sh
@@ -2,8 +2,10 @@
 . /home/assos/bin/scripts-config.sh
 
 log=$(cat)
+command="$1"
 
 current_day=$(date "+%Y-%m-%d")
+current_time=$(date "+%H-%M-%S")
 log_file="${d7_dir_log}/${current_day}.log"
 
-echo "$log" > "${log_file}"
+echo -e "%%%% ${command} — ${current_time}\n${log}\n%%%% END\n" > "${log_file}"

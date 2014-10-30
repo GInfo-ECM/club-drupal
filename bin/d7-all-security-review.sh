932c2f6c41ac726cdf120b2edfb664f3f0912225
Julien Enselme <julien.enselme@centrale-marseille.fr>
2014-10-30 10:33:54 +0100

    d7-all-security-review.sh must now use the log file.

--- bin/d7-all-security-review.sh
+++ bin/d7-all-security-review.sh
@@ -3,4 +3,4 @@
 . /home/assos/bin/scripts-config.sh
 
 # grep -v success to keep site_name and error lines.
-d7-all-drush.sh security-review --store 2>&1 | grep -v success | mail -s d7-all-security-review.sh "${email_multi_assos}"
+d7-all-drush.sh security-review --store 2>&1 | grep -v success

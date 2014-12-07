81a75448e5430f46ab0dbdb8b4fee085a5c94172
Julien Enselme <julien.enselme@centrale-marseille.fr>
2014-12-07 12:43:53 +0100

    Unlog is stable. Add alias and update log-report.sh

--- bin/log-report.sh
+++ bin/log-report.sh
@@ -4,5 +4,4 @@
 current_day=$(date "+%Y-%m-%d")
 log_file="${d7_dir_log}/${current_day}.log"
 
-cat "${log_file}" | mail -s "Log: ${current_day}" assos
-python3 /home/assos/tmp/unlog/unlog/main.py "${log_file}" --config /home/assos/.unlog  --mail-subject "Unlog: ${current_day}"
+unlog "${log_file}" --config /home/assos/.unlog  --mail-subject "Unlog: ${current_day}"

40f6eea5c84115a3e99c758c7717b9a12b17b35c
Julien Enselme <julien.enselme@centrale-marseille.fr>
2015-01-31 12:08:29 +0100

    Deletion of site: lepagetest

--- .drush/aliases.drushrc.php
+++ .drush/aliases.drushrc.php
@@ -4,7 +4,6 @@ $aliases['d7'] = array(
 'assos.centrale-marseille.fr/atrinhtest',
 'assos.centrale-marseille.fr/septiluce',
 'assos.centrale-marseille.fr/aeecm',
-'assos.centrale-marseille.fr/lepagetest',
 'assos.centrale-marseille.fr/gpeltiertest',
 'assos.centrale-marseille.fr/msb',
 'assos.centrale-marseille.fr/beret',
@@ -84,7 +83,6 @@ $aliases['ksi'] = array('uri' => 'assos.centrale-marseille.fr/ksi', 'root' => '/
 $aliases['beret'] = array('uri' => 'assos.centrale-marseille.fr/beret', 'root' => '/home/assos/drupal7/', );
 $aliases['msb'] = array('uri' => 'assos.centrale-marseille.fr/msb', 'root' => '/home/assos/drupal7/', );
 $aliases['gpeltiertest'] = array('uri' => 'assos.centrale-marseille.fr/gpeltiertest', 'root' => '/home/assos/drupal7/', );
-$aliases['lepagetest'] = array('uri' => 'assos.centrale-marseille.fr/lepagetest', 'root' => '/home/assos/drupal7/', );
 $aliases['aeecm'] = array('uri' => 'assos.centrale-marseille.fr/aeecm', 'root' => '/home/assos/drupal7/', );
 $aliases['septiluce'] = array('uri' => 'assos.centrale-marseille.fr/septiluce', 'root' => '/home/assos/drupal7/', );
 $aliases['atrinhtest'] = array('uri' => 'assos.centrale-marseille.fr/atrinhtest', 'root' => '/home/assos/drupal7/', );

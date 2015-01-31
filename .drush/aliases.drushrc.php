9449affa69940a889abbaeea302075ad7d7a8944
Julien Enselme <julien.enselme@centrale-marseille.fr>
2015-01-31 12:07:38 +0100

    Deletion of site: rbraun

--- .drush/aliases.drushrc.php
+++ .drush/aliases.drushrc.php
@@ -42,7 +42,6 @@ $aliases['d7'] = array(
         'assos.centrale-marseille.fr/mdv',
         'assos.centrale-marseille.fr/mmenossi',
         'assos.centrale-marseille.fr/phytv',
-        'assos.centrale-marseille.fr/rbraun',
         'assos.centrale-marseille.fr/tvp',
 	'assos.centrale-marseille.fr/ksi',
     ),
@@ -82,7 +81,6 @@ $aliases['jpennec'] = array('uri' => 'assos.centrale-marseille.fr/jpennec', 'roo
 $aliases['mdv'] = array('uri' => 'assos.centrale-marseille.fr/mdv', 'root' => '/home/assos/drupal7/', );
 $aliases['mmenossi'] = array('uri' => 'assos.centrale-marseille.fr/mmenossi', 'root' => '/home/assos/drupal7/', );
 $aliases['phytv'] = array('uri' => 'assos.centrale-marseille.fr/phytv', 'root' => '/home/assos/drupal7/', );
-$aliases['rbraun'] = array('uri' => 'assos.centrale-marseille.fr/rbraun', 'root' => '/home/assos/drupal7/', );
 $aliases['tvp'] = array('uri' => 'assos.centrale-marseille.fr/tvp', 'root' => '/home/assos/drupal7/', );
 $aliases['ksi'] = array('uri' => 'assos.centrale-marseille.fr/ksi', 'root' => '/home/assos/drupal7/', );
 $aliases['beret'] = array('uri' => 'assos.centrale-marseille.fr/beret', 'root' => '/home/assos/drupal7/', );
--- drupal7/sites/sites.php
+++ drupal7/sites/sites.php
@@ -83,7 +83,6 @@ $sites['assos.centrale-marseille.fr.lessive'] = 'assos.centrale-marseille.fr.les
 $sites['assos.centrale-marseille.fr.mdv'] = 'assos.centrale-marseille.fr.mdv';
 $sites['assos.centrale-marseille.fr.mmenossi'] = 'assos.centrale-marseille.fr.mmenossi';
 $sites['assos.centrale-marseille.fr.phytv'] = 'assos.centrale-marseille.fr.phytv';
-$sites['assos.centrale-marseille.fr.rbraun'] = 'assos.centrale-marseille.fr.rbraun';
 $sites['tvp.centrale-marseille.fr'] = 'assos.centrale-marseille.fr.tvp';
 $sites['assos.centrale-marseille.fr'] = 'default';
 $sites['assos.centrale-marseille.fr.charlist'] = 'assos.centrale-marseille.fr.charlist';
